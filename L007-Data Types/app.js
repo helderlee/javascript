46cda01d718513f8f00ef40c9ca63130226e1d25
Helder Lee <helderlee@yahoo.com>
2018-10-27 16:26:42 -0300

    Data Types

---
+++ L007-Data Types/app.js
@@ -0,0 +1,65 @@
+/**
+* Primitive Data Types
+*/
+
+/**
+ * string
+ */
+const name = 'John Doe';
+console.log(typeof name);
+
+/**
+ * number
+ */
+const age = 40;
+console.log(typeof age);
+
+/**
+ * boolean
+ */
+const young = true;
+console.log(typeof young);
+
+/**
+ * null
+ */
+const car = null;
+console.log(typeof car);
+
+/**
+ * undefined
+ */
+let firstName;
+console.log(typeof firstName);
+
+/**
+ * symbol (ES6)
+ */
+const aSymbol = Symbol();
+console.log(typeof aSymbol);
+
+/**
+ * Reference Data Types
+ */
+
+/**
+ * array
+ */
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+/**
+ * literal
+ */
+const address = {
+    city: 'Boston',
+    state: 'MA'
+};
+console.log(typeof address);
+
+/**
+ * date
+ */
+const today = new Date();
+console.log(typeof today);
+console.log(today);
