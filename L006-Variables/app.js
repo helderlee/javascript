ba884185cca8cebfc10c1bd53b8dd2a0ba62869f
Helder Lee <helderlee@yahoo.com>
2018-10-27 14:47:48 -0300

    Variables

--- L006-Variables/app.js
+++ L006-Variables/app.js
@@ -1,31 +1,65 @@
-/*
-* Log to console
+/**
+* Variables
 */
-console.log('Hello World');
-console.log(123);
-console.log(true);
 
-var greeting = 'Hello';
+var name = 'John Doe';
+console.log(name);
+name = 'Steve';
+console.log(name);
+
+/**
+* Init var
+*/
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
 console.log(greeting);
 
-console.log([1,2,3,4]);
+/**
+ * Letters, numbers, _, $
+ * Can not start with number
+ */
 
-console.log({a:1, b:2});
+/**
+ * Multi word vars
+ */
 
-console.table({a:1, b:2});
+var firstName = 'John'; // Camel case
+var first_name = 'Sara'; // Underscore
+var FirstName = 'Tom'; // Pascal case
 
-console.error('Some error!');
+/**
+ * Let
+ */
 
-console.clear();
+let name2 = 'John Doe';
+console.log(name2);
+name2 = 'Steve';
+console.log(name2);
 
-console.warn('Some warning!');
+/**
+ * Const
+ * Can not reassign
+ * Have to assign a value
+ */
+const constName = 'Const Name';
+console.log(constName);
 
-console.time('Timer');
-console.log('Hello World');
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.timeEnd('Timer');
+/**
+ * Contents of an object or array can be changed
+ */
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.age = 31;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+
+numbers.push(6);
+
+console.log(numbers);
