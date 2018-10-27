adb53053b726765ec3650c46978d450fe4892fa1
Helder Lee <helderlee@yahoo.com>
2018-10-27 04:19:03 -0300

    Folders renaming

---
+++ L006-Variables/app.js
@@ -0,0 +1,31 @@
+/*
+* Log to console
+*/
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+
+console.log([1,2,3,4]);
+
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+
+console.error('Some error!');
+
+console.clear();
+
+console.warn('Some warning!');
+
+console.time('Timer');
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.timeEnd('Timer');
