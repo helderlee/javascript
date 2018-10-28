2c210b36b4b024231c02605ddaa06f17af2c7c72
Helder Lee <helderlee@yahoo.com>
2018-10-27 21:32:12 -0300

    Math

---
+++ L009-Math/app.js
@@ -0,0 +1,58 @@
+/**
+ * Math
+ */
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 + num2;
+console.log(val);
+
+val = num1 * num2;
+console.log(val);
+
+val = num1 - num2;
+console.log(val);
+
+val = num1 / num2;
+console.log(val);
+
+val = num1 % num2;
+console.log(val);
+
+val = Math.PI;
+console.log(val);
+
+val = Math.E;
+console.log(val);
+
+val = Math.round(2.8);
+console.log(val);
+
+val = Math.ceil(2.4);
+console.log(val);
+
+val = Math.floor(2.8);
+console.log(val);
+
+val = Math.sqrt(64);
+console.log(val);
+
+val = Math.abs(-3);
+console.log(val);
+
+val = Math.pow(8, 2);
+console.log(val);
+
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+console.log(val);
+
+val = Math.random();
+console.log(val);
+
+val = Math.random() * 10;
+console.log(val);
+
+val = Math.floor(Math.random() * 10);
+console.log(val);
