2b8b67ba4cacf2a1db4a365fb3a2bd72db34c82c
Helder Lee <helderlee@yahoo.com>
2019-05-05 15:25:26 -0300

    Constructors

---
+++ L043-Constructors/app.js
@@ -0,0 +1,22 @@
+/**
+ * Constructors
+ */
+
+// Person constructor
+class Person {
+  constructor(name, dateOfBirthday) {
+    this.name = name;
+    this.birthday = new Date(dateOfBirthday);
+    this.age = function () {
+      const diff = Date.now() - this.birthday.getTime();
+      const ageDate = new Date(diff);
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    };
+  }
+}
+
+const brad = new Person('Brad', '1973-08-17');
+const john = new Person('John', '1980-09-13');
+
+console.log(brad.age());
+console.log(john);
