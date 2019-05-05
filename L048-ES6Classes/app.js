4daf152d267714c281c78f5efc1e13029d0bba1d
Helder Lee <helderlee@yahoo.com>
2019-05-05 17:39:57 -0300

    ES6 Classes

---
+++ L048-ES6Classes/app.js
@@ -0,0 +1,33 @@
+/**
+ * ES6 Classes
+ */
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  age () {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '1980-04-19');
+
+console.log(mary);
+console.log(mary.greeting());
+console.log(mary.age());
+
+console.log(Person.addNumbers(1,2));
