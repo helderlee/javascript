02cb684b246bdb328a022c5d66e1c22de8e98f88
Helder Lee <heldelee@yahoo.com>
2019-05-06 13:33:35 -0300

    Callback Functions

--- L062-CallbackFunctions/app.js
+++ L062-CallbackFunctions/app.js
@@ -32,16 +32,20 @@ const posts = [
 // Callback
 
 function createPost(post, callback) {
-  posts.push(post);
-  callback();
+  setTimeout(function () {
+    posts.push(post);
+    callback();
+  }, 2000); // after 2 seconds
 }
 
 function getPosts() {
-  let output = '';
-  posts.forEach(function (post) {
-    output += `<li>${post.title}</li>`;
-  });
-  document.body.innerHTML = output;
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000); // after 1 second
 }
 
 createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
