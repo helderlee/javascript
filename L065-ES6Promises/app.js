fa14573f5babb00b4f169ac2835b7446c18b47f2
Helder Lee <heldelee@yahoo.com>
2019-05-06 13:57:45 -0300

    ES6 Promises

--- L065-ES6Promises/app.js
+++ L065-ES6Promises/app.js
@@ -1,5 +1,35 @@
 const posts = [
-  {title: 'Post One', body:'This is post one'},
-  {title: 'Post Two', body: 'This is post two'}
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' }
 ];
 
+function createPost(post) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      posts.push(post);
+      const error = true;
+      //const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong');
+      }
+    }, 2000); // after 2 seconds
+  });
+}
+
+function getPosts() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000); // after 1 second
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' })
+  .then(getPosts)
+  .catch(function (err) {
+    console.log(err);
+  });
